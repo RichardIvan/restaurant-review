3b6f0087b5d579aac7f42ff36a8f8dca4b322b1c
RIdotCOM <richardivan.com@gmail.com>
2016-05-15 01:00:04 +0200

    add required files

---
+++ src/js/components/details_component.js
@@ -0,0 +1,124 @@
+'use strict'
+
+import m from 'mithril'
+import _ from 'lodash'
+
+//styles
+import style from '../../css/details.scss'
+
+//icons
+import downArrowIcon from '../../icons/rr_down_arrow.js'
+import fullStar from '../../icons/rr_review_full_star.js'
+import emptyStar from '../../icons/rr_review_empty_star.js'
+
+const getStyle = function(d) {
+  if (!d)
+    return
+
+  const dimensions = d
+
+  const position = 'absolute'
+  // const height = `${dimensions.card.height() + 16}px`
+  const height = `calc(100% - ${dimensions.card.height()}px)`
+  const width = `100%`
+  const top = `${dimensions.card.height()}px`
+
+  // TODO - DELETE
+  const background = 'white'
+
+  const zIndex = 1
+
+  return {
+    position,
+    height,
+    width,
+    top,
+    zIndex,
+    background
+  }
+}
+
+// const getPhotoStyle = (d) => {
+
+//   if (!d)
+//     return
+
+//   const dimensions = d
+
+//   const position = 'absolute'
+//   // const height = `${dimensions.card.height() + 16}px`
+//   const height = `${dimensions.card.height()}px`
+//   const width = `100%`
+//   // const top = 0
+//   const top = `-${dimensions.card.height()}px`
+
+//   // TODO - DELETE
+//   const background = 'white'
+
+//   const zIndex = 1
+
+//   return {
+//     position,
+//     height,
+//     width,
+//     top,
+//     zIndex,
+//     background
+//   }
+
+// }
+
+const generateStars = (rating) => {
+
+  const stars = new Array(rating).fill(true)
+
+  while( stars.length < 5) {
+    stars.push(false)
+  }
+
+  return stars
+} 
+
+export default {
+  controller(args) {
+
+  },
+  view(ctrl, args) {
+    console.log(args)
+    return m(`.${style['details']}`, { style: getStyle.call(null, args.dimensions) }, [
+        // m(`.${style['ptoto']}`, { style: getPhotoStyle.call(null, args.dimensions) }),
+        m(`.${style['header']}`, [
+          m('h3', 'Reviews'),
+          m(`.${style['header-opening-hours']}`, [
+            m('h3', 'Opening Hours'),
+            m('.down-arrow', downArrowIcon)
+          ])
+        ]),
+        m(`.${style['reviews']}`, [
+          m(`ul.${style['reviews-container']}`, [
+            _.map(args.restaurant.reviews, (review) => {
+              return m(`li.${style['review-item']}`, { key: review.author_name }, [
+                m(`.${style['line-one']}`, [
+                  m('h4', review.author_name),
+                  m(`h4`, review.time) 
+                ]),
+                m(`.${style['line-two']}`, [
+                  m(`.${style['clear']}`),
+                  m(`ul.${style['user-stars']}`, _.map(generateStars(review.rating), (star, i) => {
+                    switch(star) {
+                      case true:
+                        return m(`li.${style['star']}`, { key: i } ,fullStar)
+                      case false:
+                        return m(`li.${style['star']}`, { key: i } ,emptyStar)
+                    }
+                  })),
+                  m(`p`, review.text) 
+                ])
+              ])
+              console.log(review)
+            })
+          ])
+        ])
+      ])
+  }
+}
